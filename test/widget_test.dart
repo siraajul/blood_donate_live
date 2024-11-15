1b2c3010cf47d086ccd3a02d10ff5e49663d24d0
siraajul <shirajulislamparvez@gmail.com>
2024-11-15 22:06:30 +0600

    Update: widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:second_live/main.dart';
+import 'package:second_live/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
