df070c7afd4127a20d4be49b437c1584b3059122
siraajul <shirajulislamparvez@gmail.com>
2024-11-15 22:14:08 +0600

    update:--> home_page.dart

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -8,11 +8,35 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.red,
-        title: const Text('Need Blood',style: TextStyle(color: Colors.white),),
+        title: const Text(
+          'Need Blood',
+          style: TextStyle(color: Colors.white),
+        ),
         centerTitle: true,
-        actions: [
-          IconButton(onPressed: (){}, icon: const Icon(Icons.add))
-        ],
+        actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.add))],
+      ),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              radius: 90,
+              backgroundColor: Colors.black54,
+              child: Icon(
+                Icons.bloodtype_outlined,
+                size: 100,
+                color: Colors.red,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              'Donate Blood',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            )
+          ],
+        ),
       ),
     );
   }
